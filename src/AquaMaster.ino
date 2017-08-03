c3ca9bacb41243055fcefc09be318c597113901c
chipmc <chip@mcclellands.org>
2017-08-03 09:34:55 +0700

    Fixed publishing issues and added weather forecast

--- src/AquaMaster.ino
+++ src/AquaMaster.ino
@@ -5,80 +5,86 @@
  * Date: 7/18/17
  */
 
- STARTUP(WiFi.selectAntenna(ANT_AUTO)); // continually switches at high speed between antennas
- SYSTEM_THREAD(ENABLED);
+STARTUP(WiFi.selectAntenna(ANT_AUTO)); // continually switches at high speed between antennas
+SYSTEM_THREAD(ENABLED);
 
- // Finally, here are the variables I want to change often and pull them all together here
- #define SOFTWARERELEASENUMBER "0.27"
+// Software Release lets me know what version the Particle is running
+#define SOFTWARERELEASENUMBER "0.4"
 
- #include <I2CSoilMoistureSensor.h>
+// Included Libraries
+#include <I2CSoilMoistureSensor.h>   // Apollon77's Chirp Library: https://github.com/Apollon77/I2CSoilMoistureSensor
 
- I2CSoilMoistureSensor sensor;
+// Function Prototypes
+I2CSoilMoistureSensor sensor;
 
- const int solenoidPin = D2;
- const int blueLED = D7;
- const int donePin = D6;              // Pin the Electron uses to "pet" the watchdog
- const int wakeUpPin = A7;            // This is the Particle Electron WKP pin
+// Constants for Pins
+const int solenoidPin = D2;
+const int blueLED = D7;
+const int donePin = D6;              // Pin the Electron uses to "pet" the watchdog
+const int wakeUpPin = A7;            // This is the Particle Electron WKP pin
 
-
- unsigned long waterTimeShort = 60000;    // For Testing the system and smaller adjustments
- unsigned long waterTimeLong = 300000;    // The longest watering period
- unsigned long waterTime = 0;             // How long with the watering go in the main loop
- int startWaterHour = 6;                  // When can we start watering
- int stopWaterHour = 8;                   // When do we stop for the day
- char RSSIdescription[12];
- char Signal[17];
- char capDescription[12];
- char Moisture[15];
- int capValue = 0;      // This is where we store the soil moisture sensor raw data
- int wateringNow = 0;
- int waterEnabled = 1;
- int currentPeriod = 0;  // Change length of period for testing 2 times in main loop
- int lastWateredPeriod = 0; // So we only wanter once an hour
- const char* releaseNumber = SOFTWARERELEASENUMBER;  // Displays the release on the menu
- volatile bool doneEnabled = true;    // This enables petting the watchdog
- volatile bool watchdogPet = false;
+// Program variables
+unsigned long oneMinuteMillis = 60000;    // For Testing the system and smaller adjustments
+int shortWaterMinutes = 1;
+int longWaterMinutes = 5;
+unsigned long waterTime = 0;             // How long with the watering go in the main loop
+int startWaterHour = 5;                  // When can we start watering
+int stopWaterHour = 8;                   // When do we stop for the day
+char RSSIdescription[12];
+char Signal[17];
+char capDescription[12];
+char Moisture[15];
+int wateringMinutes = 0;             // How long will we water based on time or Moisture
+int capValue = 0;      // This is where we store the soil moisture sensor raw data
+int soilTemp = 0;
+int wateringNow = 0;
+int waterEnabled = 1;
+int currentPeriod = 0;  // Change length of period for testing 2 times in main loop
+int lastWateredPeriod = 0; // So we only wanter once an hour
+int lastWateredDay = 0;   // Need to reset the last watered period each day
+int currentDay = 0;
+const char* releaseNumber = SOFTWARERELEASENUMBER;  // Displays the release on the menu
+volatile bool doneEnabled = true;    // This enables petting the watchdog
+int lastWateredPeriodAddr = 0;      // Where I store the last watered period in EEPROM
+int lastWateredDayAddr = 4;
+int expectedRainfallToday = 0;
 
 void setup() {
   pinMode(donePin,OUTPUT);       // Allows us to pet the watchdog
   digitalWrite(donePin, HIGH);  // Pet now while we are getting set up
   digitalWrite(donePin, LOW);
+  attachInterrupt(wakeUpPin, watchdogISR, RISING);   // The watchdog timer will signal us and we have to respond
 
-  Particle.variable("Watering", wateringNow);
+  Particle.variable("Watering", wateringNow);       // These variables are used to monitor the device
   Particle.variable("WiFiStrength", Signal);
   Particle.variable("Moisture", Moisture);
   Particle.variable("Enabled", waterEnabled);
   Particle.variable("Release",releaseNumber);
-  Particle.function("start-stop", startStop);
+  Particle.variable("LastWater",lastWateredPeriod);
+  Particle.variable("RainFcst", expectedRainfallToday);
+  Particle.function("start-stop", startStop);       // Here are two functions for easy control
   Particle.function("Enabled", wateringEnabled);
-  Particle.subscribe("hook-response/soilMoisture", myHandler, MY_DEVICES);      // Subscribe to the integration response event
-  //Particle.subscribe("hook-response/watering", myHandler, MY_DEVICES);      // Subscribe to the integration response event
+  Particle.subscribe("hook-response/AquaMaster", dataHandler, MY_DEVICES);      // Subscribe to the integration response event
+  Particle.subscribe("hook-response/weatherU_hook", weatherHandler,MY_DEVICES); // Subscribe to weather response
 
+  Time.zone(-4);    // Raleigh DST (watering is for the summer)
 
-  Wire.begin();
+  Wire.begin();                       // Begin to initialize the libraries and devices
   Serial.begin(9600);
-  sensor.begin(); // reset sensor
+  sensor.begin();                     // reset sensor
   NonBlockingDelay(2000);
 
-  pinMode(donePin,OUTPUT);       // Allows us to pet the watchdog
-  attachInterrupt(wakeUpPin, watchdogISR, RISING);   // The watchdog timer will signal us and we have to respond
   pinMode(solenoidPin,OUTPUT);
   digitalWrite(solenoidPin, LOW);
   pinMode(blueLED,OUTPUT);
   pinMode(wakeUpPin,INPUT_PULLDOWN);   // This pin is active HIGH
 
-  Particle.connect();    // <-- now connect to the cloud, which ties up the system thread
-
+  EEPROM.get(lastWateredPeriodAddr,lastWateredPeriod);    // Load the last watered period from EEPROM
+  EEPROM.get(lastWateredDayAddr,lastWateredDay);          // Load the last watered day from EEPROM
 
   Serial.println("");                 // Header information
   Serial.print(F("AquaMaster - release "));
   Serial.println(releaseNumber);
-
-  pinMode(solenoidPin,OUTPUT);
-  digitalWrite(solenoidPin, LOW);
-  pinMode(blueLED,OUTPUT);
-
-  Time.zone(-4);    // Raleigh DST (watering is for the summer)
 
   Serial.print("I2C Soil Moisture Sensor Address: ");
   Serial.println(sensor.getAddress(),HEX);
@@ -89,31 +95,45 @@ void setup() {
 
 
 void loop() {
-  if (Time.hour() != currentPeriod)
+  if (Time.hour() != currentPeriod)         // Spring into action each hour on the hour
   {
-    getMoisture();
-    Particle.publish("Soil Temp C",String(sensor.getTemperature()/(float)10),3); //temperature register
-    getWiFiStrength();
-    currentPeriod = Time.hour();
+    Particle.publish("weatherU_hook");      // Get the weather forcast
+    NonBlockingDelay(5000);                 // Give the Weather Underground time to respond
+    getMoisture();                          // Test soil Moisture
+    getWiFiStrength();                      // Get the WiFi Signal strength
+    soilTemp = int(sensor.getTemperature()/(float)10);    // Get the Soil temperature
+    currentPeriod = Time.hour();            // Set the new current period
+    currentDay = Time.day();                // Sets the current Day
     if (waterEnabled)
     {
       if (currentPeriod >= startWaterHour && currentPeriod <= stopWaterHour)
       {
         if ((strcmp(capDescription,"Very Dry") == 0) || (strcmp(capDescription,"Dry") == 0) || (strcmp(capDescription,"Normal") == 0))
         {
-          if (currentPeriod != lastWateredPeriod)
+          if (currentPeriod != lastWateredPeriod && currentDay != lastWateredDay)
           {
-            lastWateredPeriod = currentPeriod; // Only want to water once an hour
-            if (currentPeriod == startWaterHour) waterTime = waterTimeLong;
-            else waterTime = waterTimeShort;
-            turnOnWater(waterTime);
+            if (expectedRainfallToday <= 1.1)
+            {
+              lastWateredDay = currentDay;
+              lastWateredPeriod = currentPeriod;
+              EEPROM.put(lastWateredPeriodAddr,currentPeriod);  // Sets the last watered period to the current one
+              EEPROM.put(lastWateredDay,currentDay);            // Stored in EEPROM since this issue only comes in case of a reset
+              if (currentPeriod == startWaterHour) wateringMinutes = longWaterMinutes;
+              else wateringMinutes = shortWaterMinutes;
+            }
+            else Particle.publish("Heavy Rain Expected");
           }
+          else Particle.publish("Watering","Already Watered");
         }
         else Particle.publish("Watering","Not Needed");
       }
       else Particle.publish("Watering","Not Time");
     }
     else Particle.publish("Watering","Not Enabled");
+    waterTime = wateringMinutes * oneMinuteMillis;
+    sendToUbidots();
+    if (waterTime) turnOnWater(waterTime);
+    wateringMinutes = 0;
   }
 }
 
@@ -130,11 +150,21 @@ void turnOnWater(unsigned long duration)
   Particle.publish("Watering","Done");
 }
 
+void sendToUbidots()
+{
+  //String status = "Oops";
+  //String data = String::format("{\"Moisture\":%i, \"Watering\": { \"value\": %i, \"context\": {\"status\":\"status\"}}, \"SoilTemp\":%i}",capValue, wateringMinutes, soilTemp);
+  String data = String::format("{\"Moisture\":%i, \"Watering\":%i, \"SoilTemp\":%i}",capValue, wateringMinutes, soilTemp);
+  //String data = "{\"SoilTemp\":\"19\",\"Watering\":{\"value\": \"0\", \"context\": \"Oops\"},\"Moisture\":\"516\"}";
+  Particle.publish("AquaMaster", data, PRIVATE);
+  NonBlockingDelay(1000);     //Makes sure we are spacing out our Particle.publish requests
+}
+
 int startStop(String command)   // Will reset the local counts
 {
   if (command == "start")
   {
-    turnOnWater(waterTimeShort);
+    turnOnWater(oneMinuteMillis);
     return 1;
   }
   else if (command == "stop")
@@ -205,22 +235,20 @@ int getWiFiStrength()
   strcpy(Signal,RSSIdescription);
   strcat(Signal,": ");
   strcat(Signal,String(wifiRSSI));
+  return 1;
 }
 
 
 void getMoisture()
 {
   capValue = sensor.getCapacitance();
-  String data = String(90);
-  Particle.publish("soilMoisture", data ,PRIVATE);
-  NonBlockingDelay(3000);
-  if ((capValue >= 650) || (capValue <=300))
+  if ((capValue >= 652) || (capValue <=299))
   {
     strcpy(capDescription,"Error");
   }
   else
   {
-    int strength = map(capValue, 300, 650, 0, 5);
+    int strength = map(capValue, 300, 651, 0, 5);
     switch (strength)
     {
       case 0:
@@ -251,12 +279,29 @@ void getMoisture()
 
 void NonBlockingDelay(int millisDelay)  // Used for a non-blocking delay
 {
-    unsigned long commandTime = millis();
-    while (millis() <= millisDelay + commandTime) { }
-    return;
+  unsigned long commandTime = millis();
+  while (millis() <= millisDelay + commandTime)
+  {
+    Particle.process();
+  }
+  return;
 }
 
-void myHandler(const char *event, const char *data)
+void weatherHandler(const char *event, const char *data)
+{
+  if (!data) {              // First check to see if there is any data
+    Particle.publish("Weather", "No Data");
+    return;
+  }
+  String str = String(data);
+  char strBuffer[30] = "";
+  str.toCharArray(strBuffer, 30); // example: "\"2~0~3~0~4~0~5~0.07~\""
+  int forecastDay = atoi(strtok(strBuffer, "\"~"));
+  int expectedRainfallToday = atoi(strtok(NULL, "~"));
+  if (forecastDay != currentDay) expectedRainfallToday = 0;   // In case we have the wrong day's forecast
+}
+
+void dataHandler(const char *event, const char *data)
 {
   if (!data) {              // First check to see if there is any data
     Particle.publish("WebHook", "No Data");
@@ -307,6 +352,5 @@ void watchdogISR()
   {
     digitalWrite(donePin, HIGH);
     digitalWrite(donePin, LOW);
-    watchdogPet = true;
   }
 }
